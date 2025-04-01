d00298d28e4940db9db59fc84b196ae655d2969a
davidjesus516 <davidtorcates0410@gmail.com>
2025-04-01 15:58:50 -0400

    Update basic_user_config.php

--- PROYECTO/login/controllers/login/basic_user_config.php
+++ PROYECTO/login/controllers/login/basic_user_config.php
@@ -15,11 +15,20 @@ $questions_answers = array(
     $question2 => $answer2,
     $question3 => $answer3
 );
-$row = $usuario->basic_login_config($id, $password, $questions_answers);
+$search_actual_password = $usuario->SearchUserKey($_SESSION["USER_ID"]);
+if (password_verify($_POST['passwordinput0'], $search_actual_password['KEY'])) {
+    $row = $usuario->basic_login_config($id, $password, $questions_answers);
 if ($row === true) {
     echo 1;
 } else {
     echo 0;   
 }
+}
+else {
+    echo 2; // Contraseña actual incorrecta
+}
+} else {
+    echo 0; // Error al procesar la solicitud
+
 }
 ?>
