e66b15ed017631361970e21a8a840316c0348a23
davidjesus516 <davidtorcates0410@gmail.com>
2025-04-06 21:29:52 -0400

    guardar respuestas en mayusculas

--- PROYECTO/login/controllers/login/security_question_validate.php
+++ PROYECTO/login/controllers/login/security_question_validate.php
@@ -3,8 +3,8 @@ require_once "../../model/usuario.php";
 $user_id = $_POST["user_id"];
 $question1_id = $_POST["question1_id"];
 $question2_id = $_POST["question2_id"];
-$answer1 = $_POST["answer1"];
-$answer2 = $_POST["answer2"];
+$answer1 = mb_strtoupper($_POST["answer1"]);
+$answer2 = mb_strtoupper($_POST["answer2"]);
 $user = new Usuario;
 $row = $user->userSecurityQuestionSearchByID($user_id);
 if ($row[$question1_id] == $answer1 && $row[$question2_id] == $answer2) {
