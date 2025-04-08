d95a790f22dbd2b1e0e74502bb6d8e2e870b8680
Antony Figueroa <antonysamuel0903@gmail.com>
2025-04-07 22:22:03 -0400

    creacion del modulo manuales - estatico

---
+++ PROYECTO/login/vistas/manuales.php
@@ -0,0 +1,26 @@
+<?php
+require 'header.php';
+?>
+<span class="text">Manuales</span>
+<div class="page-content">
+
+<table class="w3-table-all w3-hoverable">
+	<thead>
+		<tr class="w3-light-grey">
+			<th>Nombre</th>
+			<th colspan="2">Link</th>
+		</tr>
+        <tr>
+            <td>Manual 1</td>
+            <td><a href="https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing</a></td>
+        </tr>
+	</thead>
+	<tbody id="datos"></tbody>
+</table>
+
+
+<script src="js/jquery-3.7.0.min.js"></script>
+<!-- <script src="js/periodo.js"></script> -->
+<?php
+require 'footer.php';
+?>
