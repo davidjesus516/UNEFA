54ec29c13386f92413e07fd6feadc69b3b0d7832
Antony Figueroa <antonysamuel0903@gmail.com>
2025-04-21 19:59:55 -0400

    subir manuales (PDF)

--- PROYECTO/login/vistas/manuales.php
+++ PROYECTO/login/vistas/manuales.php
@@ -4,23 +4,40 @@ require 'header.php';
 <span class="text">Manuales</span>
 <div class="page-content">
 
-<table class="w3-table-all w3-hoverable">
-	<thead>
-		<tr class="w3-light-grey">
-			<th>Nombre</th>
-			<th colspan="2">Link</th>
-		</tr>
-        <tr>
-            <td>Manual 1</td>
-            <td><a href="https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing</a></td>
-        </tr>
-	</thead>
-	<tbody id="datos"></tbody>
-</table>
+	<table class="w3-table-all w3-hoverable">
+		<thead>
+			<tr class="w3-light-grey">
+				<th>Nombre</th>
+				<th colspan="2">Link</th>
+			</tr>
+			<tr>
+				<td>Manual 1</td>
+				<td><a href="https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Primer Ingreso</td>
+				<td><a href="https://drive.google.com/file/d/15qrASeBpcKx3_i_6t9TC9S9pXITlrrfL/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/15qrASeBpcKx3_i_6t9TC9S9pXITlrrfL/view?usp=drive_link</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Ventana de Periodo</td>
+				<td><a href="https://drive.google.com/file/d/1mcunm4_qcQWnvePp3ZAuUDEnrJaASUBb/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/1mcunm4_qcQWnvePp3ZAuUDEnrJaASUBb/view?usp=drive_link</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Ventana de Matricula</td>
+				<td><a href="https://drive.google.com/file/d/1wYFAuKfgI9dxK9MuikrGreLd8GlIrxNv/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/1wYFAuKfgI9dxK9MuikrGreLd8GlIrxNv/view?usp=drive_link</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Ventana de Carrera</td>
+				<td><a href="https://drive.google.com/file/d/15D7ycA9Ikg1IHSjP9sa1UQXNGvQKB27E/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/15D7ycA9Ikg1IHSjP9sa1UQXNGvQKB27E/view?usp=drive_link</a></td>
+			</tr>
+
+		</thead>
+		<tbody id="datos"></tbody>
+	</table>
 
 
-<script src="js/jquery-3.7.0.min.js"></script>
-<!-- <script src="js/periodo.js"></script> -->
-<?php
-require 'footer.php';
-?>
+	<script src="js/jquery-3.7.0.min.js"></script>
+	<!-- <script src="js/periodo.js"></script> -->
+	<?php
+	require 'footer.php';
+	?>
