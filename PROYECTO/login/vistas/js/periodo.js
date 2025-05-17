16765113c423cccb98ab6079f9a60e4307232ad1
luisenr14 <156227179+luisenr14@users.noreply.github.com>
2025-05-17 18:07:10 -0400

    Update periodo.js

--- PROYECTO/login/vistas/js/periodo.js
+++ PROYECTO/login/vistas/js/periodo.js
@@ -39,7 +39,7 @@ $(document).ready(function(){//aqui inicializamos javascript
             if(response==1){
                 alert('Este Nuevo Registro ha sido añadido');
             } else if (response==0) {
-                alert('Ya Este Estudiante Existe Porfavor compruebe los Registros');
+                alert('Ya Este Periodo Existe Porfavor compruebe los Registros');
             } else {
                 alert('?');
             }
@@ -72,17 +72,17 @@ $(document).ready(function(){//aqui inicializamos javascript
                 let template = '';//creo la plantilla donde imprimire los datos
                 task.forEach(task =>{//hago un array que me recorra el json y me lo imprima en el tbody
                     
-                    template +=`<tr taskid="${task.ID}">
-                        <td>${task.PERIODO}</td>
-                        <td>${task.FECHA_INICIO}</td>>
-                        <td>${task.FECHA_FIN}</td>
-                        <td>
-                            <button class="task-delete "><spam class="texto">Borrar</spam><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
-                        </td>
-                        <td>
-                            <button class="task-edit" onclick="window.dialog.showModal();"><spam class="texto">Editar</spam><spam class="icon"><svg viewBox="0 0 512 512">
-                            <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg></spam></button>
-                        </td>
+                    template +=`<tr taskid="${task.PERIOD_ID}">
+                        <td>${task.DESCRIPTION}</td>
+                        <td>${task.T_INTERNSHIPS_CODE}</td>
+                        <td>${task.START_DATE}</td>>
+                        <td>${task.END_DATE}</td>
+                        <><td>
+                                <button class="task-delete "><spam class="texto">Borrar</spam><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
+                            </td><td>
+                                    <button class="task-edit" onclick="window.dialog.showModal();"><spam class="texto">Editar</spam><spam class="icon"><svg viewBox="0 0 512 512">
+                                        <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg></spam></button>
+                                </td></>
                     </tr>
                     `
                 })
@@ -96,7 +96,7 @@ $(document).ready(function(){//aqui inicializamos javascript
         
         // Agregamos la alerta de confirmación
         if (confirm('¿Estás seguro de que deseas eliminar este registro?')) {
-            $.post('../controllers/estudiante/UserDelete.php',{id}, function (response) {
+            $.post('../controllers/periodo/UserDelete.php',{PERIOD_ID}, function (response) {
                 fetchTask();
             });
         } else {
@@ -108,18 +108,13 @@ $(document).ready(function(){//aqui inicializamos javascript
 $(document).on('click','.task-edit',function(){//escucho un click del boton task-edit que es una clase
         let element = $(this)[0].parentElement.parentElement;// accedo al elemento padre de este hasta conseguir el ID de la fila
         let id = $(element).attr('taskid');//accedo al tributo que cree que contiene la cedula que busco
-        $.post('../controllers/estudiante/UserEditSearch.php', {id}, function(response){//mando los datos al controlador
+        $.post('../controllers/periodo/UserEditSearch.php', {id}, function(response){//mando los datos al controlador
             const task = JSON.parse(response)[0]; // accede al primer objeto en el array
-            $('#id').val(task.ID).prop('readonly', true);//añado los elementos al formulario y lo hago de solo lectura
-            $('#cedula').val(task.CEDULA).prop('readonly', true);
-            $('#nombre').val(task.NOMBRE);
-            $('#apellido').val(task.APELLIDO);
-            $('#e_mail').val(task.E_MAIL);
-            $('#genero').val(task.GENERO);
-            $('#nacionalidad').val(task.NACIONALIDAD);
-            $('#tlf').val(task.TELEFONO);
-            $('#carrera').val(task.ID_CARRERA);
-            $('#turno').val(task.TURNO);
+            $('#PERIOD_ID').val(task.PERIOD_ID).prop('readonly', true);//añado los elementos al formulario y lo hago de solo lectura
+            $('#DESCRIPTION').val(task.DESCRIPTION).prop('readonly', true);
+            $('#T_INTERNSHIPS_CODE').val(task.T_INTERNSHIPS_CODE);
+            $('#START_DATE').val(task.START_DATE);
+            $('#END_DATE').val(task.END_DATE);
             edit = true;//valido la variable que esta por encima de todo para que en vez de guardar un nuevo usuario lo edite
         });
     })
