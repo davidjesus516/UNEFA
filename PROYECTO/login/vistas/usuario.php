d8e9a70ed357345930e78c1be777a3b4ad9a35da
Antony Figueroa <114882234+Antony-Figueroa@users.noreply.github.com>
2025-06-16 23:33:09 -0400

    ajuste de la ventana de config

--- PROYECTO/login/vistas/usuario.php
+++ PROYECTO/login/vistas/usuario.php
@@ -2,66 +2,65 @@
 require 'header.php';
 ?>
 
-<span class="text">Ventana -> Configuración</span>
-<div class="user-settings-page-content">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="stylesheet" href="styles/dashboard.css">
 
+<span class="text">Ventana -> Configuración</span>
+<div>
     <aside class="settings-nav">
         <ul class="settings-menu">
             <h3>USUARIO:</h3>
             <li class="menu-item active">
                 <a href="nuevo_usuario.php">
-                    <i class="icon-placeholder"></i> <span>Registro Usuarios</span>
+                    <i class="fas fa-user-plus"></i> <span>Registro Usuarios</span>
                 </a>
             </li>
-            <br>
+
             <h3>GESTIÓN USUARIO:</h3>
 
             <li class="menu-item active">
                 <a href="#">
-                    <i class="icon-placeholder"></i> <span>Gestión Usuario</span>
+                    <i class="fas fa-user-cog"></i> <span>Gestión Usuario</span>
                 </a>
             </li>
             <li class="menu-item active">
                 <a href="#">
-                    <i class="icon-placeholder"></i> <span>Gestión Roles</span>
+                    <i class="fas fa-user-shield"></i> <span>Gestión Roles</span>
                 </a>
             </li>
             <li class="menu-item active">
                 <a href="#">
-                    <i class="icon-placeholder"></i> <span>Gestión Permisos</span>
+                    <i class="fas fa-user-lock"></i> <span>Gestión Permisos</span>
                 </a>
             </li>
             <li class="menu-item active">
                 <a href="historial_session.php">
-                    <i class="icon-placeholder"></i> <span>Historial Ingreso Usuario</span>
+                    <i class="fas fa-history"></i> <span>Historial Ingreso Usuario</span>
                 </a>
             </li>
 
-            </li>
             <h3>SEGURIDAD Y ACCESO:</h3>
             <li class="menu-item active">
                 <a href="preguntas.php">
-                    <i class="icon-placeholder"></i> <span>Gestión Preguntas Preestablecidas</span>
+                    <i class="fas fa-question-circle"></i> <span>Gestión Preguntas Preestablecidas</span>
                 </a>
             </li>
             <li class="menu-item active">
                 <a href="config_contraseña.php">
-                    <i class="icon-placeholder"></i> <span>Configuración Contraseña</span>
+                    <i class="fas fa-key"></i> <span>Configuración Contraseña</span>
                 </a>
             </li>
             
             <li class="menu-item active">
                 <a href="config_preguntas.php">
-                    <i class="icon-placeholder"></i> <span>Configuración Preguntas de Seguridad</span>
+                    <i class="fas fa-shield-alt"></i> <span>Configuración Preguntas de Seguridad</span>
                 </a>
             </li>
             <li class="menu-item active">
                 <a href="config_usuario.php">
-                    <i class="icon-placeholder"></i> <span>Configuración Usuario</span>
+                    <i class="fas fa-user-edit"></i> <span>Configuración Usuario</span>
                 </a>
             </li>
-
-
         </ul>
     </aside>
 
