e8da7cfed14ecb008de624ac1314b5ba500ea370
Antony Figueroa <114882234+Antony-Figueroa@users.noreply.github.com>
2025-06-23 13:22:43 -0400

    manuales

--- PROYECTO/login/vistas/manuales.php
+++ PROYECTO/login/vistas/manuales.php
@@ -11,26 +11,53 @@ require 'header.php';
 				<th colspan="2">Link</th>
 			</tr>
 			<tr>
-				<td>Manual 1</td>
-				<td><a href="https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1YwmuTY2WxV9Q69MzNV6dSAU9i8UPHe7i/view?usp=sharing</a></td>
-			</tr>
-			<tr>
 				<td>Manual - Primer Ingreso</td>
-				<td><a href="https://drive.google.com/file/d/15qrASeBpcKx3_i_6t9TC9S9pXITlrrfL/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/15qrASeBpcKx3_i_6t9TC9S9pXITlrrfL/view?usp=drive_link</a></td>
+				<td><a href="https://drive.google.com/file/d/1yl0kguHKLmXg-LIoqVYIxeOcRxFhbcaa/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1yl0kguHKLmXg-LIoqVYIxeOcRxFhbcaa/view?usp=sharing</a></td>
 			</tr>
 			<tr>
-				<td>Manual - Ventana de Período</td>
-				<td><a href="https://drive.google.com/file/d/1mcunm4_qcQWnvePp3ZAuUDEnrJaASUBb/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/1mcunm4_qcQWnvePp3ZAuUDEnrJaASUBb/view?usp=drive_link</a></td>
+				<td>Manual - Período</td>
+				<td><a href="https://drive.google.com/file/d/1yvKymkF-EIRoEqLIY3EmSUKI9XbqUbhH/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1yvKymkF-EIRoEqLIY3EmSUKI9XbqUbhH/view?usp=sharing</a></td>
 			</tr>
 			<tr>
-				<td>Manual - Ventana de Matricula</td>
-				<td><a href="https://drive.google.com/file/d/1wYFAuKfgI9dxK9MuikrGreLd8GlIrxNv/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/1wYFAuKfgI9dxK9MuikrGreLd8GlIrxNv/view?usp=drive_link</a></td>
+				<td>Manual - Carrera</td>
+				<td><a href="https://drive.google.com/file/d/1zAbhVohKaI3d-sQOc52jNSTgrU2nM1tR/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1zAbhVohKaI3d-sQOc52jNSTgrU2nM1tR/view?usp=sharing</a></td>
 			</tr>
 			<tr>
-				<td>Manual - Ventana de Carrera</td>
-				<td><a href="https://drive.google.com/file/d/15D7ycA9Ikg1IHSjP9sa1UQXNGvQKB27E/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/15D7ycA9Ikg1IHSjP9sa1UQXNGvQKB27E/view?usp=drive_link</a></td>
+				<td>Manual - Estudiantes</td>
+				<td><a href="https://drive.google.com/file/d/1z-xB0yiy170hjkv2xgbDbWT1Nas7cApn/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1z-xB0yiy170hjkv2xgbDbWT1Nas7cApn/view?usp=sharing</a></td>
 			</tr>
-
+			<tr>
+				<td>Manual - Tutores</td>
+				<td><a href="https://drive.google.com/file/d/1zIh_FPdCm3FGtx292rHSu_eh1v0C7XRY/view?usp=drive_link" target="_blank">https://drive.google.com/file/d/1zIh_FPdCm3FGtx292rHSu_eh1v0C7XRY/view?usp=drive_link</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Instituciones</td>
+				<td><a href="https://drive.google.com/file/d/1zRIhsFWJLhy8P_X83xOyNuKMoVcO0t-f/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1zRIhsFWJLhy8P_X83xOyNuKMoVcO0t-f/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Responsables</td>
+				<td><a href="https://drive.google.com/file/d/1zJiSaIRmT-8hSHEKD0MwOp6rBqdhU8HH/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1zJiSaIRmT-8hSHEKD0MwOp6rBqdhU8HH/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Pre Inscripción</td>
+				<td><a href="https://drive.google.com/file/d/1zB1DOEI3CzTPdvh0OAFYCoZb4TLqUa53/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1zB1DOEI3CzTPdvh0OAFYCoZb4TLqUa53/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Inscripción</td>
+				<td><a href="https://drive.google.com/file/d/1zWyyrhMFMXTVxW3f9CdQ2ZZB6Vh3iumh/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1zWyyrhMFMXTVxW3f9CdQ2ZZB6Vh3iumh/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Reportes</td>
+				<td><a href="https://drive.google.com/file/d/1ypgEHKqj4v9lY9R2sfYztqd4-G5aYBhA/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1ypgEHKqj4v9lY9R2sfYztqd4-G5aYBhA/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Usuario</td>
+				<td><a href="https://drive.google.com/file/d/1zG_ZpOQCEGiFMW6XygegYQ05YESCOiZv/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1zG_ZpOQCEGiFMW6XygegYQ05YESCOiZv/view?usp=sharing</a></td>
+			</tr>
+			<tr>
+				<td>Manual - Recuperar Contraseña</td>
+				<td><a href="https://drive.google.com/file/d/1ysNpgEpoFR0EmEq5F6TxKu6gW1tYrVQX/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1ysNpgEpoFR0EmEq5F6TxKu6gW1tYrVQX/view?usp=sharing</a></td>
+			</tr>
 		</thead>
 		<tbody id="datos"></tbody>
 	</table>
